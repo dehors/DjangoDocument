f877121c15c5aa22de9617ff01c472fa46b80aec
luismartinez24 <luismartinez242011@gmail.com>
2016-03-08 12:25:56 -0600

    feat: add model to admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+class QuestionModelAdmin(admin.ModelAdmin):	
+	class Meta:
+		model = Question
+
+class ChoiceModelAdmin(admin.ModelAdmin):	
+	class Meta:
+		model = Choice
+		
+admin.site.register(Question, QuestionModelAdmin)
+admin.site.register(Choice, ChoiceModelAdmin)
+
