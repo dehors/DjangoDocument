cc5ce54232174fa5acfc9211f6d1f61099df8812
luismartinez24 <luismartinez242011@gmail.com>
2016-03-09 12:50:15 -0600

    feat: cumstom form admin (2)

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,15 +2,23 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+	model = Choice
+	extra = 3
+#lista para crear, extra cantidad de formularios 
+
 class QuestionModelAdmin(admin.ModelAdmin):	
 	fieldsets = [
 		(None,               {'fields': ['question_text']}),
-		('Date information', {'fields': ['pub_date']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+	inlines = [ChoiceInline]
+#class collapse esconde el panel
 
 class ChoiceModelAdmin(admin.ModelAdmin):	
 	class Meta:
 		model = Choice
+
 		
 admin.site.register(Question, QuestionModelAdmin)
 admin.site.register(Choice, ChoiceModelAdmin)
