78818cbc3d35a891103b1abc7d675720ac168efd
luismartinez24 <luismartinez242011@gmail.com>
2016-03-29 22:47:20 -0600

    feat: Write a simple form

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from .models import Question
 from django.template import loader
 from django.http import Http404
+from django.core.urlresolvers import reverse
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -28,4 +29,15 @@ def results(request, question_id):
 	return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	p = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (Exception, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question': p,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+	return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
