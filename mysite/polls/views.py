545c1d3c57721b19af27d34e195a92c6f7af3797
luismartinez24 <luismartinez242011@gmail.com>
2016-03-30 20:52:37 -0600

    docs: update view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,17 +16,20 @@ def index(request):
 
 def detail(request, question_id):
 	##try:
-        ##question = Question.objects.get(pk=question_id)
-    ##except Question.DoesNotExist:
-        ##raise Http404("Question does not exist")
-    ##return render(request, 'polls/detail.html', {'question': question})
+		##question = Question.objects.get(pk=question_id)
+	##except Question.DoesNotExist:
+		##raise Http404("Question does not exist")
+	##return render(request, 'polls/detail.html', {'question': question})
 
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
+
+	##response = "You're looking at the results of question %s."
+	##return HttpResponse(response % question_id)
 
 def vote(request, question_id):
 	p = get_object_or_404(Question, pk=question_id)
@@ -41,3 +44,4 @@ def vote(request, question_id):
 		selected_choice.votes += 1
 		selected_choice.save()
 	return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+	##reverse es para poner el nombre de la ruta y no la ruta
